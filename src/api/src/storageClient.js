215abefb619004be9c7aa0daceabf0478e40eccd
Raik Herrmann <raikher@microsoft.com>
2026-04-23 23:46:33 +0200

    simplify: remove auto-create table logic, tables created manually

--- src/api/src/storageClient.js
+++ src/api/src/storageClient.js
@@ -1,42 +1,20 @@
-const { TableClient, TableServiceClient } = require("@azure/data-tables");
+const { TableClient } = require("@azure/data-tables");
 const { DefaultAzureCredential } = require("@azure/identity");
 
-const tableCache = {};
+const credential = new DefaultAzureCredential();
 
-function getAccountUrl() {
+function getTableClient(tableName) {
   const name = process.env.STORAGE_ACCOUNT_NAME;
-  if (!name) throw new Error("STORAGE_ACCOUNT_NAME environment variable is not set");
-  return `https://${name}.table.core.windows.net`;
+  if (!name) throw new Error("STORAGE_ACCOUNT_NAME is not set");
+  const url = `https://${name}.table.core.windows.net`;
+  return new TableClient(url, tableName, credential);
 }
 
-let credential;
-function getCredential() {
-  if (!credential) credential = new DefaultAzureCredential();
-  return credential;
-}
-
-async function getTableClient(tableName) {
-  if (tableCache[tableName]) {
-    return tableCache[tableName];
-  }
-  const url = getAccountUrl();
-  const cred = getCredential();
-  const serviceClient = new TableServiceClient(url, cred);
-  try {
-    await serviceClient.createTable(tableName);
-  } catch (e) {
-    if (e.statusCode !== 409) throw e; // 409 = already exists
-  }
-  const client = new TableClient(url, tableName, cred);
-  tableCache[tableName] = client;
-  return client;
-}
-
-async function getRoundsTable() {
+function getRoundsTable() {
   return getTableClient("rounds");
 }
 
-async function getParticipantsTable() {
+function getParticipantsTable() {
   return getTableClient("participants");
 }
 
